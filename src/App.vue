5c9f31b0edce821cdfa9ce14d4b96208dea48647
thomas <thomaselmoleung@gmail.com>
2020-03-21 01:55:33 -0400

    added vuetify drawer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" absolute permanent>
+      <v-list-item class="px-2">
+        <v-icon class="pr-2 pl-2">mdi-desktop-mac-dashboard</v-icon>
+        <v-list-item-title>COVID19 Dashboard</v-list-item-title>
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+      </v-list-item>
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon :class="{'mt-1': !item.mdi}">
+            <v-icon v-if="item.mdi">{{ item.icon }}</v-icon>
+            <p v-else class="headline">{{ item.icon }}</p>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content class="ml-12 pl-8">
+      <Header />
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "./components/Header";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "App",
+  components: {
+    Header
+  },
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Global", icon: "mdi-earth", mdi: true },
+        { title: "Canada", icon: "🇨🇦", mdi: false },
+        { title: "About", icon: "mdi-information", mdi: true }
+      ],
+      mini: true
+    };
+  }
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col class="mb-4">
+      <h1 class="font-weight-bold mb-3">COVID-19 Dashboard</h1>
+      <p class="subheading font-weight-regular">
+        For help and collaboration with other Vuetify developers,
+        <br />please join our online
+        <a
+          href="https://community.vuetifyjs.com"
+          target="_blank"
+        >Discord Community</a>
+      </p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: () => ({})
+};
+</script>
+
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Dosis&display=swap");
+
+h1 {
+  font-family: "Dosis", sans-serif;
+}
+</style>
