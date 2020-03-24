c043814f47ceed491cfec0699538397f363e8616
thomas <thomaselmoleung@gmail.com>
2020-03-23 20:09:59 -0400

    grid can data bind html code

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid-content">
-    <h1>Hello</h1>
+    <h1>Overview</h1>
     <grid-layout
       :layout.sync="layout"
       :col-num="12"
@@ -20,7 +20,11 @@
         :h="item.h"
         :i="item.i"
         :key="item.i"
-      >{{item.i}}</grid-item>
+      >
+        <v-sheet color="grey lighten-3" height="100%">
+          <div v-html="item.i"></div>
+        </v-sheet>
+      </grid-item>
     </grid-layout>
   </div>
 </template>
@@ -40,22 +44,7 @@ export default {
         { x: 2, y: 0, w: 2, h: 4, i: "1" },
         { x: 4, y: 0, w: 2, h: 5, i: "2" },
         { x: 6, y: 0, w: 2, h: 3, i: "3" },
-        { x: 8, y: 0, w: 2, h: 3, i: "4" },
-        { x: 10, y: 0, w: 2, h: 3, i: "5" },
-        { x: 0, y: 5, w: 2, h: 5, i: "6" },
-        { x: 2, y: 5, w: 2, h: 5, i: "7" },
-        { x: 4, y: 5, w: 2, h: 5, i: "8" },
-        { x: 6, y: 3, w: 2, h: 4, i: "9" },
-        { x: 8, y: 4, w: 2, h: 4, i: "10" },
-        { x: 10, y: 4, w: 2, h: 4, i: "11" },
-        { x: 0, y: 10, w: 2, h: 5, i: "12" },
-        { x: 2, y: 10, w: 2, h: 5, i: "13" },
-        { x: 4, y: 8, w: 2, h: 4, i: "14" },
-        { x: 6, y: 8, w: 2, h: 4, i: "15" },
-        { x: 8, y: 10, w: 2, h: 5, i: "16" },
-        { x: 10, y: 4, w: 2, h: 2, i: "17" },
-        { x: 0, y: 9, w: 2, h: 3, i: "18" },
-        { x: 2, y: 6, w: 2, h: 2, i: "19" }
+        { x: 8, y: 0, w: 2, h: 3, i: `<h1>Hello</h1>` }
       ]
     };
   }
